62654439f264977b5412e3c79a61f2898cd51216
KevinChica1128 <kevin.chica@correounivalle.edu.co>
2018-03-10 18:19:04 -0500

    codigo listo

--- codigo numeros aleatorios por cuadrados medios.R
+++ codigo numeros aleatorios por cuadrados medios.R
@@ -1,14 +1,15 @@
-x<-558956 #Número al azar de minimo 4 cifras, con cifras pares (2n cifras)  
+x<-55895649 #Número al azar de minimo 4 cifras, con cifras pares (2n cifras)  
 cifrasinicial<-trunc(log10(x))+1    #Detectar cifras del número incial
 n<-cifrasinicial/2 #encontramos el n de la formula 2n
-N<-22 #Cantidad de números aleatorios a generar
+N<-1000 #Cantidad de números aleatorios a generar
 u<-c()
 for (i in 1:N) {
   x<-x^2
   cif<-trunc(log10(x))+1
-  if((cif)!=4*n) {
+  while((cif)!=4*n) {
     x<-x*(10)
-  }
+    cif<-trunc(log10(x))+1
+    }
   xnueva<-toString(x)
   numeroscentrales<-substring(xnueva,n+1,(2*n+n))
   num<-(as.numeric(numeroscentrales))/10^cifrasinicial
