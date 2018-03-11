3c844c4ec4a51d366e18072349efb5b8a364e9d1
Cesar Saavedra <Andrs917@gmail.com>
2018-03-10 21:50:41 -0500

    Codigo

--- Tarea3.R
+++ Tarea3.R
@@ -1,4 +1,4 @@
-#Comportamiento distribución Poisson:
+#Comportamiento distribuci?n Poisson:
 #Generamos poblaciones:
 n<-10000
 pb1<-rpois(n,1)
@@ -19,9 +19,9 @@ plot(p4,type = "h",xlab = "x",ylab = "Frecuencia",main="Poisson(50)")
 
 
 #-----------------------------------------------------#
-#Comportamiento distribución Logistica:
+#Comportamiento distribuci?n Logistica:
 #Generamos poblaciones:
-#Variando el parametro de escala y dejando la localización en 0:
+#Variando el parametro de escala y dejando la localizaci?n en 0:
 pl1<-rlogis(n,0,1)
 pl2<-rlogis(n,0,5)
 pl3<-rlogis(n,0,10)
@@ -40,7 +40,7 @@ hist(pl3,freq = F,xlab = "x",ylab = "Frecuencia",main="Logistic(0,10)")
 lines(l3,dlogis(l3,0,10))
 hist(pl4,freq = F,xlab = "x",ylab = "Frecuencia",main="Logistic(0,50)")
 lines(l4,dlogis(l4,0,50))
-#Variando el parametro de escala y dejando la localización en 0:
+#Variando el parametro de escala y dejando la localizaci?n en 0:
 pob1<-rlogis(n,1,1)
 pob2<-rlogis(n,5,1)
 pob3<-rlogis(n,10,1)
@@ -74,3 +74,32 @@ plot(density(pob1),xlim=c(-5,80))
 lines(density(pob2))
 lines(density(pob3))
 lines(density(pob4))
+
+#--------------------------------------------------------------------------------------#
+x<-558956 #N??mero al azar de minimo 4 cifras, con cifras pares (2n cifras)  
+cifrasinicial<-trunc(log10(x))+1    #Detectar cifras del n??mero incial
+n<-cifrasinicial/2 #encontramos el n de la formula 2n
+N<-1000 #Cantidad de n??meros aleatorios a generar
+u<-c()
+for (i in 1:N) {
+  x<-x^2
+  cif<-trunc(log10(x))+1
+  while((cif)!=4*n) {
+    x<-x*(10)
+    cif<-trunc(log10(x))+1
+  }
+  xnueva<-toString(x)
+  numeroscentrales<-substring(xnueva,n+1,(2*n+n))
+  num<-(as.numeric(numeroscentrales))/10^cifrasinicial
+  u[i]<-c(num)
+  x<-as.numeric(numeroscentrales)
+}
+
+x11()
+plot(u)
+
+print(u)
+
+
+
+
